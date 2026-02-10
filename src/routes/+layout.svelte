ce09c29a1ffc9dd4af3a6a548dc86cca656bd53c
Nathan Hoyt <slimnatekc@gmail.com>
2026-02-10 00:54:35 -0600

    Add backdrop blur to top nav buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,21 +11,23 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="flex min-h-screen min-w-full flex-col bg-[radial-gradient(ellipse_at_bottom,var(--color-midnight-700)_0%,var(--color-midnight-900)_100%)]">
-<StarBackground />
+<div
+	class="flex min-h-screen min-w-full flex-col bg-[radial-gradient(ellipse_at_bottom,var(--color-midnight-700)_0%,var(--color-midnight-900)_100%)]"
+>
+	<StarBackground />
 	<!-- Header buttons -->
 	<div class="relative z-10 mx-auto w-full">
 		<div class="flex items-center justify-center p-12">
 			<ButtonGradientHover
-				classes="w-40 rounded-l-full border-2 border-r border-midnight-400 py-2 pr-2 pl-4 text-center text-xl group hover:border-midnight-200 transition-border-color duration-300"
+				classes="w-40 rounded-l-full border-2 border-r border-midnight-400 py-2 pr-2 pl-4 text-center text-xl group hover:border-midnight-200 transition-border-color duration-300 backdrop-blur-[2px]"
 				gradientColor="var(--color-midnight-500)"
 				href="/"
 			>
-				<BounceText>Portfolio</BounceText>
+				<BounceText classes="">Portfolio</BounceText>
 			</ButtonGradientHover>
 
 			<ButtonGradientHover
-				classes="w-40 rounded-r-full border-2 border-l border-midnight-400 py-2 pr-4 pl-2 text-center text-xl group hover:border-midnight-200 transition-border-color duration-300"
+				classes="w-40 rounded-r-full border-2 border-l border-midnight-400 py-2 pr-4 pl-2 text-center text-xl group hover:border-midnight-200 transition-border-color duration-300 backdrop-blur-[2px]"
 				gradientColor="var(--color-midnight-500)"
 				href="/resume"
 			>
